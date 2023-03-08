d937df404f85bfb2aa812398ee2b659deeb66edf
Matheus Vinicius Bianchini <mvbbianchini@hotmail.com>
2023-03-08 20:37:58 -0300

    feat: added base url to instrumental test

--- app/src/androidTest/java/com/example/marvelapp/presentation/characters/CharactersFragmentTest.kt
+++ app/src/androidTest/java/com/example/marvelapp/presentation/characters/CharactersFragmentTest.kt
@@ -1,21 +1,23 @@
 package com.example.marvelapp.presentation.characters
 
-import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.marvelapp.framework.di.BaseUrlModule
 import com.example.marvelapp.launchFragmentInHiltContainer
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
+@UninstallModules(BaseUrlModule::class)
 @RunWith(AndroidJUnit4::class)
 @HiltAndroidTest
 class CharactersFragmentTest {
 
     @get:Rule
-        var hiltRule = HiltAndroidRule(this)
+    var hiltRule = HiltAndroidRule(this)
 
     @Before
     fun setup() {
