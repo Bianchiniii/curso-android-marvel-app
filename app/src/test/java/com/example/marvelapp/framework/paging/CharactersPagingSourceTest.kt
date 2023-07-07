a9bf9b6615cde0e9ec4d712103d148d93461815f
Matheus Vinicius Bianchini <mvbbianchini@hotmail.com>
2023-07-06 21:55:01 -0300

    test: fixed unitary test

--- app/src/test/java/com/example/marvelapp/framework/paging/CharactersPagingSourceTest.kt
+++ app/src/test/java/com/example/marvelapp/framework/paging/CharactersPagingSourceTest.kt
@@ -2,14 +2,13 @@ package com.example.marvelapp.framework.paging
 
 import androidx.paging.PagingSource
 import com.bianchini.vinicius.matheus.core.data.repository.CharactersRemoteDataSource
-import com.example.marvelapp.factory.response.DataWrapperResponseFactory
-import com.example.marvelapp.framework.network.response.DataWrapperResponse
+import com.example.marvelapp.factory.response.CharacterPagingResponseFactory
 import com.exemple.testing.MainCoroutineRule
 import com.exemple.testing.model.CharacterFactory
 import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.whenever
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.test.runTest
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
@@ -18,20 +17,20 @@ import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
 
+@ExperimentalCoroutinesApi
 @RunWith(MockitoJUnitRunner::class)
 class CharactersPagingSourceTest {
 
     //regra que inclue a dependencia do coroutines para inserir na thread principal e limpar após o fim
-    @ExperimentalCoroutinesApi
     @get:Rule
     var mainCoroutineRule = MainCoroutineRule()
 
     private lateinit var charactersPagingSource: CharactersPagingSource
 
-    private val dataWrapperResponse = DataWrapperResponseFactory()
+    private val dataWrapperResponse = CharacterPagingResponseFactory()
 
     @Mock
-    lateinit var charactersRemoteDataSource: CharactersRemoteDataSource<DataWrapperResponse>
+    lateinit var charactersRemoteDataSource: CharactersRemoteDataSource
 
     private val characterFactory = CharacterFactory()
 
@@ -40,9 +39,8 @@ class CharactersPagingSourceTest {
         charactersPagingSource = CharactersPagingSource(charactersRemoteDataSource, "")
     }
 
-    @ExperimentalCoroutinesApi
     @Test
-    fun `should return a success load result when load is called`() = runBlockingTest {
+    fun `should return a success load result when load is called`() = runTest {
         //arange
         whenever(charactersRemoteDataSource.fetchCharacters(any())).thenReturn(
             dataWrapperResponse.create()
@@ -73,9 +71,8 @@ class CharactersPagingSourceTest {
         )
     }
 
-    @ExperimentalCoroutinesApi
     @Test
-    fun `should return a error result when load is called`() = runBlockingTest {
+    fun `should return a error result when load is called`() = runTest {
         //arrange
         val runtimeException = RuntimeException()
         whenever(charactersRemoteDataSource.fetchCharacters(any())).thenThrow(
