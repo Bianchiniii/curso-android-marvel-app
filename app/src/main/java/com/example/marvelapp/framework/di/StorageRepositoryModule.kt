f2d3a8fd6908e241362f841d13e8bbdcebc9dc88
Matheus Vinicius Bianchini <mvbbianchini@hotmail.com>
2024-06-24 21:25:55 -0300

    feat: added preferences data source files

---
+++ app/src/main/java/com/example/marvelapp/framework/di/StorageRepositoryModule.kt
@@ -0,0 +1,21 @@
+package com.example.marvelapp.framework.di
+
+import com.bianchini.vinicius.matheus.core.data.repository.StorageLocalDataSource
+import com.bianchini.vinicius.matheus.core.data.repository.StorageRepository
+import com.example.marvelapp.framework.StorageRepositoryImpl
+import com.example.marvelapp.framework.local.DataStorePreferencesDataSource
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface StorageRepositoryModule {
+
+    @Binds
+    fun bindsStorageRepository(impl: StorageRepositoryImpl): StorageRepository
+
+    @Binds
+    fun bindsLocalDataSource(impl: DataStorePreferencesDataSource): StorageLocalDataSource
+}
